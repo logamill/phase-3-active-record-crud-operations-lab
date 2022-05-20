96e73d608aeab8f2992f7f1d61c22033d9ce6756
Logan Miller <loganmiller214@gmail.com>
2022-05-20 12:57:48 -0400

    commit tree 37273bb4a4fb62b50bd90d65c52d72ef77c84956

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,48 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
+
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count 
+        Movie.count 
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find_by(id: id)
+    end
+
+    def self.find_movie_with_attributes(info)
+        Movie.find_by info 
+    end
+
+    def self.find_movies_after_2002
+        Movie.where "release_date > 2002"
+    end
+
+    def update_with_attributes(info)
+        self.update(info)
+    end
+
+    def self.update_all_titles(title)
+        Movie.update(title: title)
+    end
+
+    def self.delete_by_id(id)
+        movie = Movie.find(id)
+        Movie.delete(id)
+    end
+
+    def self.delete_all_movies
+        Movie.destroy_all
+    end
+
 end
